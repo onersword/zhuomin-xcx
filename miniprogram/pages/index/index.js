b7fd4a102cb243495466e134dc5067100ee77a0b
tony <tony@woo.network>
2025-02-27 20:48:06 +0800

    fix

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,7 +1,6 @@
 // index.js
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {
       avatarUrl: '',
       nickName: ''
@@ -11,36 +10,36 @@ Page({
     canIUseNicknameComp: true,
     calendarConfig: {
       hideHeader: true,
-      weekMode:false,
+      weekMode: false,
     },
     targetDate: 1735660800000,  // 2025-01-01 00:00:00
     healthManageList: [
-          {
-            title: '健康小结',
-            time: 1705385100000, // 2025.1.16 14:25
-            url: '/pages/health/summary/index'
-          },
-          {
-            title: '您也需要一个家庭医生',
-            time: 1705385100000,
-            url: '/pages/health/doctor/index'
-          },
-          {
-            title: '多维度全面健康评估服务包',
-            time: 1705385100000,
-            url: '/pages/health/assessment/index'
-          },
-          {
-            title: '慢性病管理 报告',
-            time: 1705385100000,
-            url: '/pages/health/chronic/index'
-          },
-          {
-            title: '2025年一月健康小结',
-            time: 1705385100000,
-            url: '/pages/health/monthly/index'
-          }
-        ],
+      {
+        title: '健康小结',
+        time: 1705385100000, // 2025.1.16 14:25
+        url: '/pages/health/summary/index'
+      },
+      {
+        title: '您也需要一个家庭医生',
+        time: 1705385100000,
+        url: '/pages/health/doctor/index'
+      },
+      {
+        title: '多维度全面健康评估服务包',
+        time: 1705385100000,
+        url: '/pages/health/assessment/index'
+      },
+      {
+        title: '慢性病管理 报告',
+        time: 1705385100000,
+        url: '/pages/health/chronic/index'
+      },
+      {
+        title: '2025年一月健康小结',
+        time: 1705385100000,
+        url: '/pages/health/monthly/index'
+      }
+    ],
     purchasedServices: [
       {
         id: 1,
@@ -52,17 +51,35 @@ Page({
         id: 2,
         name: '2025年度体检计划',
         description: '在科学管理的决策中走一步来参与课题主手作用组织学习服管理理论的新方法和新理论',
-        icon:  '../../images/service2.png'// 这里使用一个占位图片
+        icon: '../../images/service2.png'// 这里使用一个占位图片
       }
     ]
   },
-  
+
   onLoad() {
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true
       })
+
     }
+    // 查看是否授权
+    wx.getSetting({
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: (res) => {
+              console.log(res.userInfo)
+              this.setData({
+                userInfo: res.userInfo,
+                hasUserInfo: true
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
   goToSchedule() {
@@ -84,21 +101,8 @@ Page({
     })
   },
 
-  onChooseAvatar(e) {
-    const { avatarUrl } = e.detail
-    this.setData({
-      "userInfo.avatarUrl": avatarUrl
-    })
-  },
 
-  onInputChange(e) {
-    const nickName = e.detail.value
-    this.setData({
-      "userInfo.nickName": nickName
-    })
-  },
-
-  onShow: function() {
+  onShow: function () {
     if (typeof this.getTabBar === 'function' &&
       this.getTabBar()) {
       this.getTabBar().setData({
@@ -106,5 +110,5 @@ Page({
       })
     }
 
-},
+  },
 }) 
