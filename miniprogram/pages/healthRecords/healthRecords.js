ef17e1470e66a0b6e648b733607a2c003de5dce8
tony <tony@woo.network>
2025-03-27 22:10:35 +0800

    fix

--- miniprogram/pages/healthRecords/healthRecords.js
+++ miniprogram/pages/healthRecords/healthRecords.js
@@ -1,29 +1,14 @@
 const { request } = require('../../utils/request')
 Page({
   data: {
-    userInfo: {
-      recordId: 'ZMCX2025001',
-      createDate: '2025-01-02',
-      name: '饶宇',
-      gender: '男',
-      nationality: '中国',
-      birthDate: '1976-11-06',
-      idNumber: '3623***001X',
-      address: '-----',
-      phone: '-----'
-    },
-    medicalInfo: {
-      insurance: '医保',
-      medicalCare: '医保',
-      occupation: '退休',
-      emergencyContact: 'Sasa',
-      relationship: '配偶',
-      emergencyPhone: '-----',
-      emergencyBirthDate: '1976-11-06',
-      emergencyIdNumber: '3623***001X',
-      emergencyAddress: '-----',
-      emergencyContactPhone: '-----'
-    }
+    hasRecords: false,
+    recordInfo: [{
+      label: '档案编号',
+      value: 'ZMCX2025001'
+    }, {
+      label: '建档日期',
+      value: '2025-01-02'
+    }]
   },
 
   onShow: function () {
@@ -38,7 +23,7 @@ Page({
   },
 
   // 下载PDF功能
-  downloadPDF: function() {
+  downloadPDF: function () {
     wx.showToast({
       title: 'PDF下载中...',
       icon: 'loading'
@@ -51,5 +36,16 @@ Page({
       method: 'GET'
     })
     console.log('userRecords', data)
+    this.setData({
+      hasRecords: data && data.length > 0
+    })
+  },
+
+  createRecord() {
+    console.log('createRecord')
+    wx.openEmbeddedMiniProgram({
+      appId: 'wxebadf544ddae62cb',
+      path: 'pages/webview/index?sid=17551916&hash=d24d&navigateBackMiniProgram=true'
+    })
   }
 }); 
