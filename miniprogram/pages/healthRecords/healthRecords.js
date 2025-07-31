3dd33862ad95cc6d44adaeeeca27c49fd84c40ea
tony <tony@woo.network>
2025-07-31 22:28:29 +0800

    fix

--- miniprogram/pages/healthRecords/healthRecords.js
+++ miniprogram/pages/healthRecords/healthRecords.js
@@ -7,6 +7,41 @@ Page({
     loading: true
   },
 
+  // 脱敏处理方法
+  desensitizeData(records) {
+    return records.map(item => {
+      if (!item.value) return item
+      
+      const value = item.value.toString()
+      
+      // 手机号脱敏：前三后三
+      if (item.label === '手机号' && value.length >= 6) {
+        const prefix = value.substring(0, 3)
+        const suffix = value.substring(value.length - 3)
+        const stars = '*'.repeat(value.length - 6)
+        item.value = prefix + stars + suffix
+      }
+      
+      // 身份证号/护照号脱敏：前三后三
+      if (item.label === '身份证号/护照号' && value.length >= 6) {
+        const prefix = value.substring(0, 3)
+        const suffix = value.substring(value.length - 3)
+        const stars = '*'.repeat(value.length - 6)
+        item.value = prefix + stars + suffix
+      }
+      
+      // 紧急联系人电话脱敏：前三后三
+      if (item.label === '紧急联系人电话' && value.length >= 6) {
+        const prefix = value.substring(0, 3)
+        const suffix = value.substring(value.length - 3)
+        const stars = '*'.repeat(value.length - 6)
+        item.value = prefix + stars + suffix
+      }
+      
+      return item
+    })
+  },
+
   onShow: function () {
     if (typeof this.getTabBar === 'function' &&
       this.getTabBar()) {
@@ -18,8 +53,6 @@ Page({
     this.getUserRecords()
   },
 
-
-
   async getUserRecords() {
     this.setData({
       loading: true
@@ -30,10 +63,12 @@ Page({
     })
     console.log('userRecords', data)
     if (data.userInfo.status === 2) {
+      // 对敏感信息进行脱敏处理
+      const processedRecords = this.desensitizeData(data.records)
 
       this.setData({
         status: data.userInfo.status,
-        recordInfo:data.records,
+        recordInfo: processedRecords,
         recordPDFUrl: data.pdfUrl
       })
     } else {
