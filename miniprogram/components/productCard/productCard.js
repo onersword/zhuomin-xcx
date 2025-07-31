9be8d090700bba6b40081901a3a24832460e6747
tony <tony@woo.network>
2025-07-31 22:23:09 +0800

    fix

--- miniprogram/components/productCard/productCard.js
+++ miniprogram/components/productCard/productCard.js
@@ -7,13 +7,10 @@ Component({
       observer: function (newVal) {
         console.log('newVal', newVal)
         if (newVal) {
-          // 将时间戳转换为格式化的时间字符串
-          const formattedPrice = formatPrice(newVal.price)
 
           this.setData({
             product: {
               ...newVal,
-              price: formattedPrice
             }
           })
         }
@@ -26,10 +23,8 @@ Component({
 
   methods: {
     onTap() {
-      console.log('data', this.data.product.id, this.data.product.type)
       this.triggerEvent('cardtap', {
         id: this.data.product.id,
-        type: this.data.product.type,
         name: this.data.product.name,
         description: this.data.product.description
       })
